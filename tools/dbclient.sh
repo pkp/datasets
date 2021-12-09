8c669a45aff48768ca5a70b7d28b2f6c455a43a6
Alec Smecher <alec@smecher.bc.ca>
2021-12-09 15:59:02 -0800

    Add a tool to connect to the database client

---
+++ tools/dbclient.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# @file tools/dbclient.sh
+#
+# Copyright (c) 2014-2021 Simon Fraser University
+# Copyright (c) 2010-2021 John Willinsky
+# Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+#
+# Connect to the database client.
+#
+# Requires the following environment vars to be set:
+# - DBTYPE: Database type (MySQL/MySQLi/PostgreSQL)
+# - DBUSERNAME: Database username
+# - DBPASSWORD: Database password
+# - DBNAME: Database name
+# - DBHOST: Database hostname
+
+set -e
+
+case "$DBTYPE" in
+	PostgreSQL)
+		psql --username=$DBUSERNAME --host=$DBHOST $DBNAME
+		;;
+	MySQL|MySQLi)
+		mysql --user=$DBUSERNAME --password=$DBPASSWORD --host=$DBHOST $DBNAME
+		;;
+	*)
+		echo "Unknown DBTYPE \"${DBTYPE}\"!"
+		exit 1
+esac
