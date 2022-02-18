85bf38e397d43fec1da30f0ba6a540d714ade03e
PKP <pkp@mailinator.com>
2022-02-18 23:58:29 +0000

    Update datasets (main)

--- ojs/main/mysql/config.inc.php
+++ ojs/main/mysql/config.inc.php
@@ -51,12 +51,11 @@ session_lifetime = 30
 scheduled_tasks = Off
 
 ; Site time zone
-; Please refer to lib/pkp/registry/timeZones.xml for a full list of supported
+; Please refer to https://www.php.net/timezones for a full list of supported
 ; time zones.
-; I.e.:
-; <entry key="Europe/Amsterdam" name="Amsterdam" />
-; time_zone="Amsterdam"
-time_zone = "UTC"
+; I.e.: "Europe/Amsterdam"
+; time_zone="Europe/Amsterdam"
+time_zone = UTC
 
 ; Short and long date formats
 date_format_short = "%Y-%m-%d"
@@ -195,9 +194,6 @@ web_cache_hours = 1
 
 ; Default locale
 locale = en_US
-
-; Client output/input character set
-client_charset = utf-8
 
 ; Database connection character set
 connection_charset = utf8
