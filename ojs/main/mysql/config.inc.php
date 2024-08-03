1b8febbf0abb0fd4dbeaaf352170c41517c36d89
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2024-08-03 05:35:42 +0000

    Update datasets ( ojs main mysql)

--- ojs/main/mysql/config.inc.php
+++ ojs/main/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:mf9zC/5DL85J9CmDzCN4Q+PFFR90AzH0eiWec3VGhdQ="
+app_key = "base64:ZrsS96+XXysh0RClmzCBB3EdU22HlodZCa2P9U2GSt0="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -565,6 +565,11 @@ job_runner_max_execution_time = 30
 ; When setting a fixed value in megabytes, this should be less than the
 ; memory_limit the server has configured for PHP.
 job_runner_max_memory = 80
+
+; Controls whether queued jobs should be processed by the task scheduler.
+; This setting has no effect when the job_runner and the [schedule].task_runner are enabled,
+; on this situation the jobs will be processed solely by the job runner.
+process_jobs_at_task_scheduler = Off
 
 ; Remove failed jobs from the database after the following number of days.
 ; Remove this setting to leave failed jobs in the database.
