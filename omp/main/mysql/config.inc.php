a6501415a486c2c77970c9df4d0fbed43e34ff08
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2024-11-06 05:44:28 +0000

    Update datasets ( omp main mysql)

--- omp/main/mysql/config.inc.php
+++ omp/main/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:vkdraBTVf9U2FxXwFcEksifg+w2+EAeRYayvEZwn5ZY="
+app_key = "base64:EbXFSU3JOaBa5krLPsu4zeRMpr7TQTk1e09raygVxtk="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -295,17 +295,17 @@ allowed_title_html = "b,i,u,sup,sub"
 
 ; Default method to send emails
 ; Available options: sendmail, smtp, log, phpmailer
-default = sendmail
+default = smtp
 
 ; Path to the sendmail, -bs argument is for using SMTP protocol
 sendmail_path = "/usr/sbin/sendmail -bs"
 
 ; Use SMTP for sending mail instead of mail()
-; smtp = On
+smtp = On
 
 ; SMTP server settings
-; smtp_server = mail.example.com
-; smtp_port = 25
+smtp_server = localhost
+smtp_port = 1025
 
 ; Enable SMTP authentication
 ; Supported smtp_auth: ssl, tls (see PHPMailer SMTPSecure)
