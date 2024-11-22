d251b7ab2904bef29335dff7d0476d076bb0dc41
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2024-11-22 05:43:31 +0000

    Update datasets ( ojs main mariadb)

--- ojs/main/mariadb/config.inc.php
+++ ojs/main/mariadb/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:FS077lsJixD5kVnNxi55TfRjyfj5hIZweJ3UX8BVVB4="
+app_key = "base64:gdwVG/p3kSlFHOcR55eK/FPpaFuHMJ8RSoSpgGGBo9I="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
