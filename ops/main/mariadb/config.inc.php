a44439a52d5e3d0661306928e46b7d2ef628fe99
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2025-02-12 05:28:51 +0000

    Update datasets ( ops main mariadb)

--- ops/main/mariadb/config.inc.php
+++ ops/main/mariadb/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:c0+rGNgmPQKTCg+6lEHreGBN2sHafcnEVla9bUCNGHQ="
+app_key = "base64:uiCuw5RCl+0nZrKwP29eeTtSCCLWppcQTXNnW841Wcs="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -162,8 +162,8 @@ debug = Off
 [cache]
 
 ; Default driver for Laravel-based caching. Currently supports opcache and file drivers.
-; By default, the opcache is used.
-; default = opcache
+; By default, the file is used.
+; default = file
 
 ; Path to store cache contents for file or opcode based caches.
 ; path = cache/opcache
