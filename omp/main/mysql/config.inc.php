186779475324f0f3f2a046888bb6efa5797787e3
pkp-machine-user <asmecher+200411@users.noreply.github.com>
2025-03-15 05:46:10 +0000

    Update datasets ( omp main mysql)

--- omp/main/mysql/config.inc.php
+++ omp/main/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:thc1EQ8MGgDOOsaTRJNJ/NMRLrF2NYKjKW4UPWcSOXA="
+app_key = "base64:gB2jGt5X8V5ct79QMlpQAIb2yifOTfn7ZCoATfpGhMA="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
