7076ff421c715ffb35e340c13bd7f7e501e5e60d
pkp-machine-user <asmecher+200411@users.noreply.github.com>
2025-03-19 05:42:33 +0000

    Update datasets ( omp stable-3_5_0 mysql)

--- omp/stable-3_5_0/mysql/config.inc.php
+++ omp/stable-3_5_0/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:ATFCF+f5InD3dbqHZuT6HswPaaKdoz4XaOpmtPRpKGw="
+app_key = "base64:ckw6oeLBNnlQuKrSoUyzcErW0QBE0gf6jZ/Y7IX0U6U="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -630,5 +630,3 @@ expiration_days = 3
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 [features]
-
-enable_new_submission_listing = On
