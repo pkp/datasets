df91adbfc7f3475dd0558436289fb9ad1ec09ec8
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2025-03-19 05:54:29 +0000

    Update datasets ( ops main pgsql)

--- ops/main/pgsql/config.inc.php
+++ ops/main/pgsql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:cEmjirnUlJyPJ+SA/W1VdXxHaPZgrv7HTslFXBPZ64Y="
+app_key = "base64:ELgpQvodfshf575NSO8wdP8lf6+kEPE9DSIbN97pZsY="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -622,5 +622,3 @@ expiration_days = 3
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
 [features]
-
-enable_new_submission_listing = On
