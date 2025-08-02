e4264ba617e60dd13d7462aa50784ad84da9556a
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2025-08-02 05:59:40 +0000

    Update datasets ( ojs main mariadb)

--- ojs/main/mariadb/config.inc.php
+++ ojs/main/mariadb/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:LY7qSNHAQRWJ6ctE4KYJQEDpHhVGKvEsZd7bGmmPz3M="
+app_key = "base64:Gw4LxoMKexLOpzCaeyehCUAOCZYZGBxAoe6tP4gz3Kw="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -361,6 +361,18 @@ validation_timeout = 14
 ;;;;;;;;;;;;;;;;;;;
 
 [search]
+
+; The search driver to use. Options: database, opensearch
+driver = database
+
+; For indexing engines supporting it, provide a name for the submission index.
+search_index_name = "submissions"
+
+; When using the OpenSearch driver, the following must be specified...
+;opensearch_hosts = "[\"https://localhost:9200\"]"
+;opensearch_username = "admin"
+;opensearch_password = "password"
+;opensearch_ssl_verification = On
 
 ; Minimum indexed word length
 min_word_length = 3
