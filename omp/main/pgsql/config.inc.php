ab79167e18c935ee4a5f50851553589915978c81
pkp-machine-user <asmecher+200411@users.noreply.github.com>
2025-09-24 05:53:30 +0000

    Update datasets ( omp main pgsql)

--- omp/main/pgsql/config.inc.php
+++ omp/main/pgsql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:iCsM9v2iZbhQNxoNHPiiRNCkGyC4yxUQyQ0SmVWAFBM="
+app_key = "base64:lsGUhuSlp9DCWvaHSBorh/M66efXf2QYGlV9Hjr6UH0="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
