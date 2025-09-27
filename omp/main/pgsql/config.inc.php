6f72e2b098043dd14afc48685d64bb3e1419dec2
pkp-machine-user <asmecher+200411@users.noreply.github.com>
2025-09-27 05:51:56 +0000

    Update datasets ( omp main pgsql)

--- omp/main/pgsql/config.inc.php
+++ omp/main/pgsql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:K554gkkb22LXAIecYkux1qG3WDrDySGKUcRkrvV+FmU="
+app_key = "base64:lX5OvqK6qI3oDGbgN0VY0ng/lW4h5W5yfzTqkKdR0BI="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -38,6 +38,10 @@ base_url = "http://localhost"
 ; Enable strict mode. This will more aggressively cause errors/warnings when
 ; deprecated behaviour exists in the codebase.
 strict = Off
+
+; Sentry error logging support (see https://sentry.io)
+; To enable Sentry support, configure the Sentry DSN here.
+; sentry_dsn = ""
 
 ; Session cookie name
 session_cookie_name = OMPSID
