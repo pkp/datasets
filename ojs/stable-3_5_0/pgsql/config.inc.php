726d84d54116c8915267cc5f36f3c7076634fb84
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2025-11-28 06:10:56 +0000

    Update datasets ( ojs stable-3_5_0 pgsql)

--- ojs/stable-3_5_0/pgsql/config.inc.php
+++ ojs/stable-3_5_0/pgsql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:uHyeDYbYMqJGOB9iBobcNgOuqW7JdOI69y4j9TEANhQ="
+app_key = "base64:uE2DZjIL3ZdHehVDEiu6xsrzRN+Tj8Rw3sLtDAkry48="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -591,7 +591,7 @@ delete_failed_jobs_after = 180
 ; Sample for the *nix crontab:
 ; * * * * * php lib/pkp/tools/scheduler.php run >> /dev/null 2>&1
 ;
-; See: <link-to-documentation>
+; See: https://docs.pkp.sfu.ca/admin-guide/en/deploy-scheduled-tasks
 task_runner = On
 
 ; How often the built-in scheduled task runner should run at the
