c697b22fdd2e03b97df477427580cdafb2168ec2
pkp-machine-user <asmecher+200411@users.noreply.github.com>
2026-01-23 05:57:29 +0000

    Update datasets ( omp main mysql)

--- omp/main/mysql/config.inc.php
+++ omp/main/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:Dp8w/1h08E45o1yuKsBCSucvnXOhoQ8oDEQ1tF7oBYE="
+app_key = "base64:GvvOEAFMV6YXmitHbAwVA2UoDpBVwrZdUwtrQqmZBYQ="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -434,6 +434,9 @@ items_per_page = 25
 
 ; Number of page links to display; can be overridden on a per-press basis
 page_links = 10
+
+; Maximum nesting depth for navigation menu items (default: 2)
+navigation_menu_max_depth = 2
 
 
 ;;;;;;;;;;;;;;;;;;;;
