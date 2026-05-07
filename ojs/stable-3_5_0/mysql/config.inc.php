ede5d2169a78cbf06f9040ec9cb32d3cfa4db136
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2026-05-07 07:13:37 +0000

    Update datasets ( ojs stable-3_5_0 mysql)

--- ojs/stable-3_5_0/mysql/config.inc.php
+++ ojs/stable-3_5_0/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:HxkviwDZMAVBPda5fa6BgOHSLk/4CboVv8wJKc3hJmk="
+app_key = "base64:f85+kRQ+efiVyehOOm5MdWQhK99KszxwHgRHrNPh35Y="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
