d964ccc6336a15fb09d0a169466a835b3bbc3c40
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2026-08-07 06:31:54 +0000

    Update datasets ( ops main pgsql)

--- ops/main/pgsql/config.inc.php
+++ ops/main/pgsql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:D1uaxcgALvopsUE4PQ5YZxz+YbuFr6ZvlNGVP/NmTkg="
+app_key = "base64:u4M+9WY8FkgMy9EeRKNSTuV1XIbWI/yTHEWRHmiR8D8="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -597,7 +597,7 @@ log_web_service_info = Off
 ;   - syslog: System log
 ;   - errorlog: PHP error log
 ;   - null: Discard all logs
-log_channel = daily
+log_channel = errorlog
 
 ; Minimum log level to record. Available levels (in order of severity):
 ;   - debug: Detailed debug information
