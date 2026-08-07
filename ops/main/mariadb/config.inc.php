52c88921d49e252e7409f8396f969c4f71786731
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2026-08-07 06:31:27 +0000

    Update datasets ( ops main mariadb)

--- ops/main/mariadb/config.inc.php
+++ ops/main/mariadb/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:Uh6R7frUj+x2d/qSRwhYIrLT7H9XU8R4Ma0hU1K1wE8="
+app_key = "base64:Xq6PFRwcPtdQkF8xJde9lxPXTxa5/STI7ZLTKdI02t8="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -597,7 +597,7 @@ log_web_service_info = Off
 ;   - syslog: System log
 ;   - errorlog: PHP error log
 ;   - null: Discard all logs
-log_channel = daily
+log_channel = errorlog
 
 ; Minimum log level to record. Available levels (in order of severity):
 ;   - debug: Detailed debug information
