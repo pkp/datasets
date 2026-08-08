c9fdb8f4369c1c0697e59d14ce68f9ea167092c1
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2026-08-08 06:10:06 +0000

    Update datasets ( ops main mysql)

--- ops/main/mysql/config.inc.php
+++ ops/main/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:jJRDhcnm482eTtelWDjf5IMSYF/414k4whZ2iF8jI4Q="
+app_key = "base64:qDux413avYJJtgp5D3j34v0upkw4m56m3ZVkdpvnPpo="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
@@ -597,7 +597,7 @@ log_web_service_info = Off
 ;   - syslog: System log
 ;   - errorlog: PHP error log
 ;   - null: Discard all logs
-log_channel = daily
+log_channel = errorlog
 
 ; Minimum log level to record. Available levels (in order of severity):
 ;   - debug: Detailed debug information
