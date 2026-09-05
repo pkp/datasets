62e8a5c33396fc095ef9196e57dcbe4aa828e42f
pkp-machine-user <asmecher+200411@users.noreply.github.com>
2026-09-05 09:40:02 +0000

    Update datasets ( omp main mariadb)

--- omp/main/mariadb/config.inc.php
+++ omp/main/mariadb/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:9qI7BWR0HF6yPgkAf8hJeWzLuFb96D0Ro4oKS3ujnu8="
+app_key = "base64:B8Mx/ulyR/xfSlXnvo60SfT962ffq2cU+Tka9v8LWVU="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
