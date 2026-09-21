e742c0680eae26a4f57721865e684d9d1390aadd
pkp-machine-user <withanage+1921992@users.noreply.github.com>
2026-09-21 11:09:16 +0000

    Update datasets ( ops main mysql)

--- ops/main/mysql/config.inc.php
+++ ops/main/mysql/config.inc.php
@@ -26,7 +26,7 @@
 
 ; An application-specific key that is required for the app to run
 ; Internally this is used for any encryption (specifically cookie encryption if enabled)
-app_key = "base64:uaE/BXWL8HxazKW0Ugj/tcWnHRMGQZV4QV8YtjZSa/U="
+app_key = "base64:oSOd54IhEHINU1RI+xP8nEuUdbGCDw9y2hsczsI9V+A="
 
 ; Set this to On once the system has been installed
 ; (This is generally done automatically by the installer)
